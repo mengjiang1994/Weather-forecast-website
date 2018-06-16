f7ebeb5eb9744cd105afeea1da78cc9b52dd217e
Meng Jiang <mengjiang@uq.net.au>
2018-06-16 12:09:03 +1000

    update the feature: switch to celsius or fahrenheit via radio

--- src/weather/Toolbar.js
+++ src/weather/Toolbar.js
@@ -4,11 +4,20 @@ export default function Toolbar(props) {
     return (
         <React.Fragment>
             <nav>
-                <input type='text' value ={props.city} id='city'/>
+                <input value ={props.city}
+                        onChange={props.setState}/>
+                 
+                 
                  <button onClick={props.onRefresh}>Load</button>
+                 
             </nav>
-      </React.Fragment>
+       </React.Fragment>
         
+        // <nav>
+        //     <input value={this.state.curCity}
+        //            onChange={(e) => this.setState({curCity:e.target.value})} />
+        //     <button onClick={() => {this.onRefresh() } }>Load</button>
+        //   </nav>
           
        
       
--- src/weather/WeatherChannel.js
+++ src/weather/WeatherChannel.js
@@ -15,6 +15,7 @@ export default class WeatherChannel extends Component {
         super(props);
         // use static data to fill initial state first
         this.state = {
+          unit: 'f',
           curCity: 'brisbane',
           conditionData: {
                 // city:  '',
@@ -35,19 +36,64 @@ export default class WeatherChannel extends Component {
       const conditionData ={
         city: data.display_location.full,
         weather: data.weather,
-        temp: data.temp_c
+        temp: data.temp_c + "°C",
       }
+
       this.setState({conditionData});
+      
     }
 
+    handleConditionDataF(data){
+      //console.log("got data from api:",data);
+      const conditionData ={
+        city: data.display_location.full,
+        weather: data.weather,
+        temp: data.temp_f + "°F",
+      }
+
+      this.setState({conditionData});
+      
+    }
     // onSubmit() {
     //   alert('Clicked');
     //   fetchConditionData(this.state.curCity, (data) => {this.handleCondtionData(data)});
     // }
 
-    onRefresh() {
-      //alert('Clicked');
+    switchC(data) {
       fetchConditionData(this.state.curCity, (data) => {this.handleConditionData(data)});
+      // const conditionData ={
+      //   city: data.display_location.full,
+      //   weather: data.weather,
+      //   temp: data.temp_c + "°C",
+      // }
+
+      // this.setState({conditionData});
+      
+    
+    }
+
+    switchF(data) {
+      fetchConditionData(this.state.curCity, (data) => {this.handleConditionDataF(data)});
+      fetchForecastData(this.state.curCity, (forecast) => {
+        const data = forecast.map(item => {
+          return {weekday: item.date.weekday_short, hight: item.high.fahrenheit, low: item.low.fahrenheit, icon_url: item.icon_url
+          }
+        })
+      this.setState({days: data});
+      });
+    
+    }
+
+    onRefresh() {
+      console.log(document.querySelector('input[name=tempunit]:checked').value);
+      fetchConditionData(this.state.curCity, (data) => {this.handleConditionData(data)});
+      fetchForecastData(this.state.curCity, (forecast) => {
+        const data = forecast.map(item => {
+          return {weekday: item.date.weekday_short, hight: item.high.celsius, low: item.low.celsius, icon_url: item.icon_url
+          }
+        })
+      this.setState({days: data});
+      });
     }
 
     componentDidMount() {
@@ -68,9 +114,18 @@ export default class WeatherChannel extends Component {
       return (
         <main>
           <nav>
-            <input value={this.state.curCity}
+             <input value={this.state.curCity}
                    onChange={(e) => this.setState({curCity:e.target.value})} />
-            <button onClick={() => {this.onRefresh() } }>Load</button>
+              <button onClick={() => {this.onRefresh() } }>Load</button>
+              <form>
+                    <input type="radio" id="celsius" name="tempunit" value="celsius" checked={this.state.unit === 'celsius'} onChange={(e) => this.setState({ unit: e.target.value })} onClick={() => {this.onRefresh() }} />°C
+                    <input type="radio" id="fahrenheit" name="tempunit" value="fahrenheit" checked={this.state.unit === 'fahrenheit'} onChange={(e) => this.setState({ unit: e.target.value })} onClick={(data) => {this.switchF(data) } } />°F
+              </form>
+            {/* <input type="radio" value="c" checked={true} />Celsius
+            <input type="radio" value="f" />Fahrenheit */}
+
+           
+
           </nav>
           {/* <section id="Toolbar">
             <Toolbar city={this.state.curCity} onRefresh={() => this.onRefresh()}/>
